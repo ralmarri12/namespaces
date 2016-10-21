6ec80e8dff37d818b5d4989b341a93de04c32ab3
Rashid F <r.almarri12@gmail.com>
2016-10-21 18:56:23 +0800

    upgrade the architecture.

---
+++ app/greedy/Component/Model.php
@@ -0,0 +1,15 @@
+<?php
+namespace Greedy\Component;
+use Illuminate\Database\Eloquent\Model as Model;
+
+/**
+ *
+ */
+class GModel extends Model
+{
+
+    function __construct(argument)
+    {
+        # code...
+    }
+}
