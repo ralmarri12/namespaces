0aa0dac4bceb1fc424cfd53e7bc44cd82fbbd83e
Rashid F <r.almarri12@gmail.com>
2016-10-21 18:24:42 +0800

    Repository clean & initialize Component package.

---
+++ app/greedy/Component/Component.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Greedy\Component;
+/**
+ * @ auth: Rashid F.
+ * @lastupdated 21/10/2016
+ */
+class Component
+{
+    private     $request;
+
+    protected   $name,
+                $display_name;
+
+    function __construct()
+    {
+        # code...
+    }
+}
---
+++ app/greedy/Component/Controller.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Greedy\Component;
+
+/**
+ * @ auth: Rashid F.
+ * @lastupdated 21/10/2016
+ */
+
+class Controller
+{
+
+    function __construct()
+    {
+        # code...
+    }
+
+    function render_view(View $view, $data = [])
+    {
+        // TODO body of Controller->render_view(View, array()) function.
+    }
+
+    function modelizer(Model $model, Request $request)
+    {
+        // TODO body of Controller->modelizer(Model, Request) function.
+    }
+}
